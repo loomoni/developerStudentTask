82f9396b56e3d825a65cb2b959dd0553903b4737
unknown <loomonimorwo1@gmail.com>
2020-10-19 00:37:22 +0300

    developer app

---
+++ app/src/main/java/com/example/developerstudentcommunicationapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.developerstudentcommunicationapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragmet(new LoginFragment());
+        pagerAdapter.addFragmet(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        public AuthenticationPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        void addFragmet(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
+    }
+
+    public void homePage(View v){
+        Intent i = new Intent(MainActivity.this,
+                StudentHome.class);
+        startActivity(i);
+    }
+    public void doneReg(View v){
+        Toast toast = Toast.makeText(MainActivity.this, "Congratulation you have been registerd in devstudent App", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+
+}
